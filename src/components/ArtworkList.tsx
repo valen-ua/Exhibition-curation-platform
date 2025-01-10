4bee95ad361266293c4595dc20c4a3b4c9207611
Valentyna Borysenko <valentynaborysenko11@gmail.com>
2025-01-10 19:46:16 +0200

    added basic error handling

--- src/components/ArtworkList.tsx
+++ src/components/ArtworkList.tsx
@@ -3,67 +3,74 @@ import { fetchArtworks } from "../utils/wellcomeCollectionApi";
 import { fetchArtworksFromChicago } from "../utils/chicagoArtInstituteApi";
 
 interface Artwork1 {
-    id: string;
+  id: string;
   aspectRatio: number;
   averageColor: string;
   thumbnail: {
     url: string;
     credit: string;
-    }
-    source: {
-        id: string;
-        title: string;
-        type: string;
-      };
-  }
+  };
+  source: {
+    id: string;
+    title: string;
+    type: string;
+  };
+}
 
-  interface Artwork2 {
-    image_id: string;
+interface Artwork2 {
+  image_id: string;
   title: string;
   artist: string;
-  }
+}
 export const MultiApiFetch = () => {
-  const [allWellcomeArtworks, setAllWellcomeArtworks] = useState <Artwork1[]>([]);
-  const [allChicagoArtworks, setAllChicagoArtworks] = useState <Artwork2[]>([])
+  const [allWellcomeArtworks, setAllWellcomeArtworks] = useState<Artwork1[]>(
+    []
+  );
+  const [allChicagoArtworks, setAllChicagoArtworks] = useState<Artwork2[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     Promise.all([
-        fetchArtworks().then((AllWellcomeArtworks) => {
-            setAllWellcomeArtworks(AllWellcomeArtworks);     
-          }),
-          
-        fetchArtworksFromChicago().then((AllChicagoArtworks) => {
-            const mappedChicagoArtworks = AllChicagoArtworks
-            .map((artwork: any) => ({
-                image_id: artwork.image_id,
-              title: artwork.title || "Unknown Title",
-              artist: artwork.artist_title ||  "Unknown Artist"
-            }));
-            setAllChicagoArtworks(mappedChicagoArtworks)
+      fetchArtworks()
+        .then((AllWellcomeArtworks) => {
+          setAllWellcomeArtworks(AllWellcomeArtworks);
         })
-    ])
+        .catch((error) =>
+          console.error("Error fetching Wellcome artworks:", error)
+        ),
+
+      fetchArtworksFromChicago()
+        .then((AllChicagoArtworks) => {
+          const mappedChicagoArtworks = AllChicagoArtworks.map(
+            (artwork: any) => ({
+              image_id: artwork.image_id,
+              title: artwork.title || "Unknown Title",
+              artist: artwork.artist_title || "Unknown Artist",
+            })
+          );
+          setAllChicagoArtworks(mappedChicagoArtworks);
+        })
+        .catch((error) =>
+          console.error("Error fetching Chicago artworks:", error)
+        ),
+    ]);
     setIsLoading(false);
-    
   }, []);
   const constructImageUrl = (infoUrl: string, size = "300, 300") => {
-    
     return infoUrl.replace("/info.json", `/full/${size}/0/default.jpg`);
   };
 
-  
   if (isLoading) {
     return <h3>Loading...</h3>;
   }
   return (
     <div>
-     
       <div className="wellcome-artworks">
         {allWellcomeArtworks.map((artwork) => (
           <div key={artwork.id} className="artwork-item">
             <img
               src={constructImageUrl(artwork.thumbnail.url)}
               alt={artwork.source.title}
-              style={{ maxWidth: "300px", height: "300px", margin: "10px"  }}
+              style={{ maxWidth: "300px", height: "300px", margin: "10px" }}
             />
             <h4>{artwork.source.title}</h4>
             <p>Credit: {artwork.thumbnail.credit}</p>
@@ -71,11 +78,10 @@ export const MultiApiFetch = () => {
         ))}
       </div>
 
-      
       <div className="chicago-artworks">
-      {allChicagoArtworks.map((artwork, index) => (
+        {allChicagoArtworks.map((artwork, index) => (
           <div key={index} className="artwork-item">
-              {artwork.image_id ? (
+            {artwork.image_id ? (
               <img
                 src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`}
                 alt={artwork.title}
@@ -92,7 +98,5 @@ export const MultiApiFetch = () => {
     </div>
   );
 };
-
-
 
 export default MultiApiFetch;
