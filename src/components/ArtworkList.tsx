5b4b608405d33a5042e42f8646b84bc7542a680f
Valentyna Borysenko <valentynaborysenko11@gmail.com>
2025-01-12 15:52:10 +0200

    Added styling for  ArtworkList component

--- src/components/ArtworkList.tsx
+++ src/components/ArtworkList.tsx
@@ -2,55 +2,58 @@ import { useState, useEffect } from "react";
 import { fetchArtworks } from "../utils/wellcomeCollectionApi";
 import { fetchArtworksFromChicago } from "../utils/chicagoArtInstituteApi";
 
-interface Artwork1 {
-  id: string;
-  aspectRatio: number;
-  averageColor: string;
+interface WellcomeArtwork {
   thumbnail: {
     url: string;
     credit: string;
   };
+  id: string;
   source: {
-    id: string;
     title: string;
-    type: string;
   };
 }
 
-interface Artwork2 {
+interface ChicagoArtwork {
   image_id: string;
   title: string;
   artist: string;
 }
 export const MultiApiFetch = () => {
-  const [allWellcomeArtworks, setAllWellcomeArtworks] = useState<Artwork1[]>(
-    []
-  );
-  const [allChicagoArtworks, setAllChicagoArtworks] = useState<Artwork2[]>([]);
+  const [allWellcomeArtworks, setAllWellcomeArtworks] = useState<
+    WellcomeArtwork[]
+  >([]);
+  const [allChicagoArtworks, setAllChicagoArtworks] = useState<
+    ChicagoArtwork[]
+  >([]);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     Promise.all([
       fetchArtworks()
-        .then((AllWellcomeArtworks) => {
-          setAllWellcomeArtworks(AllWellcomeArtworks);
+        .then((allWellcomeArtworks) => {
+          setAllWellcomeArtworks(
+            allWellcomeArtworks.map((artwork: WellcomeArtwork) => ({
+              id: artwork.id,
+              thumbnail: artwork.thumbnail,
+              source: artwork.source,
+            }))
+          );
         })
         .catch((error) =>
-          console.error("Error fetching Wellcome artworks:", error)
+          console.error("Error fetching Wellcome Collection artworks:", error)
         ),
 
       fetchArtworksFromChicago()
-        .then((AllChicagoArtworks) => {
-          const mappedChicagoArtworks = AllChicagoArtworks.map(
-            (artwork: any) => ({
+        .then((allChicagoArtworks) =>
+          setAllChicagoArtworks(
+            allChicagoArtworks.map((artwork: any) => ({
               image_id: artwork.image_id,
               title: artwork.title || "Unknown Title",
               artist: artwork.artist_title || "Unknown Artist",
-            })
-          );
-          setAllChicagoArtworks(mappedChicagoArtworks);
-        })
+            }))
+          )
+        )
         .catch((error) =>
-          console.error("Error fetching Chicago artworks:", error)
+          console.error("Error fetching Chicago Art Institute artworks:", error)
         ),
     ]);
     setIsLoading(false);
@@ -64,36 +67,50 @@ export const MultiApiFetch = () => {
   }
   return (
     <div>
-      <div className="wellcome-artworks">
-        {allWellcomeArtworks.map((artwork) => (
-          <div key={artwork.id} className="artwork-item">
-            <img
-              src={constructImageUrl(artwork.thumbnail.url)}
-              alt={artwork.source.title}
-              style={{ maxWidth: "300px", height: "300px", margin: "10px" }}
-            />
-            <h4>{artwork.source.title}</h4>
-            <p>Credit: {artwork.thumbnail.credit}</p>
-          </div>
-        ))}
-      </div>
+      <div className="container">
+        <div className="artworks-wrapper">
+          <div className="grid">
+            <div className="wellcome-artworks">
+              {allWellcomeArtworks.map((artwork) => (
+                <div key={artwork.id} className="artwork-item">
+                  <img
+                    src={constructImageUrl(artwork.thumbnail.url)}
+                    alt={artwork.source.title}
+                    style={{
+                      maxWidth: "300px",
+                      height: "300px",
+                      margin: "10px",
+                    }}
+                  />
+                  <h4>{artwork.source.title}</h4>
+                  <p>Credit: {artwork.thumbnail.credit}</p>
+                </div>
+              ))}
+            </div>
 
-      <div className="chicago-artworks">
-        {allChicagoArtworks.map((artwork, index) => (
-          <div key={index} className="artwork-item">
-            {artwork.image_id ? (
-              <img
-                src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`}
-                alt={artwork.title}
-                style={{ maxWidth: "300px", height: "300px", margin: "10px" }}
-              />
-            ) : (
-              <p>No image available</p>
-            )}
-            <h4>{artwork.title}</h4>
-            <p>Artist: {artwork.artist}</p>
+            <div className="chicago-artworks">
+              {allChicagoArtworks.map((artwork, index) => (
+                <div key={index} className="artwork-item">
+                  {artwork.image_id ? (
+                    <img
+                      src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`}
+                      alt={artwork.title}
+                      style={{
+                        maxWidth: "300px",
+                        height: "300px",
+                        margin: "10px",
+                      }}
+                    />
+                  ) : (
+                    <p>No image available</p>
+                  )}
+                  <h4>{artwork.title}</h4>
+                  <p>Artist: {artwork.artist}</p>
+                </div>
+              ))}
+            </div>
           </div>
-        ))}
+        </div>
       </div>
     </div>
   );
