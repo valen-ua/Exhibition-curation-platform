66c28a3bd6bb354e83b7a84c0f1262b6fe651b07
Valentyna Borysenko <valentynaborysenko11@gmail.com>
2025-01-31 12:15:15 +0200

    added Home component with basic information on the project

---
+++ src/components/Home.tsx
@@ -0,0 +1,38 @@
+export const Home = () => {
+  return (
+    <div className="home-page-container">
+        <div className="content-box">
+      <h2 className="home-header">Welcome to your Exhibition Curator</h2>
+      <p>
+        Discover and curate art like never before! Our platform allows you to
+        explore artworks from the <b>Wellcome Collection</b> and <b>The Art Institute of
+        Chicago</b>, using open-access APIs to bring you a diverse range of
+        historical and contemporary pieces.
+      </p>
+      <h4>Key features</h4>
+      <ul className="home-page-list">
+        <li>
+        ✅<b>Easy Browsing</b> - Browse artworks using the "Load More" button bottom of
+          the page
+        </li>
+        <li>
+        ✅<b>Search Across Collections</b> - Find artworks searching by name or author
+        </li>
+        <li>
+        ✅<b>Filter artworks</b> - Quickly refine your search by source or choose to
+          only see artworks that are in public domain
+        </li>
+        <li>
+        ✅<b>Detailed Artwork View</b> - See high-quality images and essential details
+          about each piece.
+        </li>
+        <li>
+        ✅<b>Curate Your Own Exhibition</b> - Save artworks to a personal, temporary
+          exhibition and manage your collection effortlessly.
+        </li>
+      </ul>
+      <h3>Start exploring today and build your own art exhibition! 🖼️✨</h3>
+    </div>
+    </div>
+  );
+};
