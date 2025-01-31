0a49cf79025fbf190a8828ab71d865211cf8b6db
Valentyna Borysenko <valentynaborysenko11@gmail.com>
2025-01-31 12:14:20 +0200

    updated with Home and Exhibitions paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,39 @@
+import "./App.css";
+import MultiApiFetch from "./components/ArtworkList";
+import { Header } from "./components/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { SearchApiFetch } from "./components/Search";
+import { ArtworkDetail } from "./components/ArtworkDetails";
+import { ExhibitionProvider } from "./components/ExhibitionContext";
+import ExhibitionList from "./components/ExhibitionList";
+import ExhibitionDetail from "./components/ExhibitionDetails";
+import { NavigationBar } from "./components/NavBar";
+import { Home } from "./components/Home";
 
-import './App.css'
-import MultiApiFetch from './components/ArtworkList'
-import { Header } from './components/Header'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { SearchApiFetch } from './components/Search'
-import { ArtworkDetail } from './components/ArtworkDetails'
-
-function App()  {
-  
+function App() {
   return (
     <>
-      <div className='body'>
-      <Header />
-      <Router>
-        <Routes>
-    <Route path="/" element={<MultiApiFetch />} />
-    <Route path="/search-results" element={<SearchApiFetch />} />
-    <Route path="/artwork/:artworkId" element={<ArtworkDetail />} />
-     </Routes>
-    </Router>
+      <div className="body">
+        <Header />
+        <ExhibitionProvider>
+          <Router>
+            <NavigationBar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/artworks" element={<MultiApiFetch />} />
+              <Route path="/search-results" element={<SearchApiFetch />} />
+              <Route path="/artwork/:artworkId" element={<ArtworkDetail />} />
+              <Route path="/exhibitions" element={<ExhibitionList />} />
+              <Route
+                path="/exhibitions/:exhibitionId"
+                element={<ExhibitionDetail />}
+              />
+            </Routes>
+          </Router>
+        </ExhibitionProvider>
       </div>
-     
-     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
