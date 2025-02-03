7d7cda5281305fb9b2f5d3ad2705983109ba2350
Valentyna Borysenko <valentynaborysenko11@gmail.com>
2025-02-03 13:13:58 +0200

    Added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+
+export const Footer = () => {
+    return (
+        <div className="footer-text">
+        <h6>This web application has been developed as part of the Launchpad project by <a href="https://www.techreturners.com/">Tech Returners</a></h6>
+        </div>
+    )
+}
